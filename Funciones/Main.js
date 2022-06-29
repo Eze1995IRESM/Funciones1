4f5111c5fb7ce06307736875ef5bd62398a04685
Eze1995IRESM <74332400+Eze1995IRESM@users.noreply.github.com>
2022-06-29 20:18:01 -0300

    Add files via upload

---
+++ Funciones/Main.js
@@ -0,0 +1,25 @@
+/*function mostrarNumero (){
+
+    let numero= 5;
+    console.log(mostrarNumero);
+    mostrarNumero();
+}
+*/
+
+/*const sumar=(a,b)=> (a + b);
+console.log(sumar(3,4))*/
+
+/*const nombreApellido=(nombre, apellido) => (nombre + apellido)
+console.log(nombreApellido('Pistolero ', ' Suarez'))
+nombreApellido();*/
+
+/*let numero=2;
+const numeroMayorMenor=(numero)=>(numero);{
+    if(numero>0 || numero==0){
+        console.log('El numero es positivo')}
+        else{
+            console.log('El numero es negativo')
+        }
+
+console.log(numeroMayorMenor(3))
+}*/
